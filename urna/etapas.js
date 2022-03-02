a55bdb7edd4a4f7b7c3ffc9405d0a3b3f16c40af
DIOGODTF <dtf.diogo@gmail.com>
2022-03-02 13:14:04 -0300

    Update repository

---
+++ urna/etapas.js
@@ -0,0 +1,47 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'38111',
+                nome:'Fulano de Tal',
+                partido:'ABC',
+                fotos:[{url:'38111.jpg', legenda:'Vereador', small: false}]
+            },
+            {
+                numero:'77222',
+                nome:'Beltrano da Silva',
+                partido:'DFGF',
+                fotos:[{url:'77222.jpg', legenda:'Vereador'}]
+            }
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros:2,
+        candidatos:[
+            {
+                numero:'99',
+                nome:'Ciclano',
+                partido:'ABC',
+                vice:'Ciclano',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero:'84',
+                nome:'Zilano',
+                partido:'QWEABC',
+                vice:'Zulano no',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+
+        ]
+    }
+];
