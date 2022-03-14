0930dc3abf3d5ca7b6a3ec9dd1dd3c800cb789de
DIOGODTF <dtf.diogo@gmail.com>
2022-03-14 07:04:32 -0300

    captura

---
+++ iniciando js/assets/js/script.js
@@ -0,0 +1,14 @@
+let nome = 'diogo'
+
+let ingredientes = [
+   'acucar',
+   'farinha de trigo',
+   'margarina',
+   '3 ovos',
+   'leite',
+   'corante azul'
+];
+
+for (let i=0; i < ingredientes.length  ;i++){
+  //console.log( ingredientes[i] );
+}
